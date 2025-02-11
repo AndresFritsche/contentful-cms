aa6ec9d34057f4ec3eb03f65c157a8ff67cd0a09
Andres Fritsche <andresf0090@gmail.com>
2025-02-11 16:49:29 -0400

    added useFetchProjects custom hook

--- src/fetchProjects.jsx
+++ src/fetchProjects.jsx
@@ -1,10 +1,33 @@
 import { createClient } from "contentful";
+import { useState, useEffect } from "react";
 
 const client = createClient({
   space: "9llnn87k571",
   environment: "master",
   accessToken: "eTx2XZtMObOFCVgwDWIpVRTh8GNKvKqlieH4FsCMt-A",
 });
-client
-  .getEntries({ content_type: "projects" })
-  .then((response) => console.log(response));
+
+const useFetchProjects = () =>{
+    const [loading,setLoading] = useState(true);
+    const [projects,setProjects] = useState([]);
+    const getData = async ()=>{
+        try {
+            const response = await client
+            .getEntries({ content_type: "projects" });
+            setLoading(false)
+        } catch (error) {
+            console.log(error);
+            setLoading(false)
+            
+        }
+    }
+
+    useEffect(()=>{
+        getData()
+    },[])
+
+}
+
+
+
+
