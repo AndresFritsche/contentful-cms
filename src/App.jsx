0d0d3868035c026874b983fc0c52d74e520a7eec
Andres Fritsche <andresf0090@gmail.com>
2025-02-12 15:39:06 -0400

    added services to main app component

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,12 @@
 import Hero from "./Hero";
 import Projects from "./Projects";
+import Services from "./Services";
 
 const App = () => {
   return <main>
     <Hero />
     <Projects />
-    
+    <Services />
   </main>;
 };
 export default App;
